04c903dcd27c6c0743d754be1fb92e8bac8b2b96
Luc Mosser <luc.mosser86@orange.fr>
2020-07-07 15:27:22 +0200

    add styled component

---
+++ gatsby-browser.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme"
+
+const GlobalStyles = createGlobalStyle `
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body, html {
+    font-family: ${props => props.theme.fonts.main};
+    height: 100%;
+    background-color: red;
+  }
+`
+
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
+  )
---
+++ src/themes/theme.js
@@ -0,0 +1,7 @@
+export default {
+  fonts: {
+    main: "Alice",
+    code: "Roboto Mono, monospace",
+  },
+
+}
